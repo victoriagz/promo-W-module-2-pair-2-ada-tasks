d3723ae247f8f3684742b323c8296184bb1d944f
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-02-26 13:05:13 +0100

    add tasks

--- main.js
+++ main.js
@@ -2,37 +2,48 @@
 
 const list = document.querySelector('.js-taks-list');
 const inputAdd = document.querySelector('.js-input-add');
-const buttonAdd = document.querySelector ('.js-add-btn');
+const buttonAdd = document.querySelector('.js-add-btn');
 
 
-let inputValue; 
+let inputValue;
 
 
 const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-  ];
+  { name: 'Recoger setas en el campo', completed: true },
+  { name: 'Comprar pilas', completed: true },
+  { name: 'Poner una lavadora de blancos', completed: true },
+  {
+    name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+    completed: false,
+  },
+];
 
+console.log(tasks);
 
-function taksPrint(tasks) {
-    for (const items of tasks){
-        list.innerHTML += `<li><input type="checkbox"> ${items.name} </li> `
-    }
+for (const items of tasks) {
+  list.innerHTML += `<li><input type="checkbox"> ${items.name} </li> `
 }
 
-taksPrint (tasks);
+function taksPrint(tasks) {
 
-
-
-inputAdd.addEventListener('input', () => { 
+  inputAdd.addEventListener('input', () => {
     inputValue = inputAdd.value;
-    buttonAdd.addEventListener('click', () => {
-        
-    })
+  });
+  buttonAdd.addEventListener('click', (event) => {
+    event.preventDefault();
+    list.innerHTML = ` `
+    tasks.push({ name: inputValue });
+    console.log(tasks);
 
-});
+    for (const items of tasks) {
+      list.innerHTML += `<li><input type="checkbox"> ${items.name} </li> `
+    }
+  })
+
+}
+
+taksPrint(tasks);
+
+
+
+
